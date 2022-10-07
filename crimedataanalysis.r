5501939818ca264680ba504332c1401ab467fe98
github_Prateek90000 <pg21prateek.dadhich@isbm.ac.in>
2022-10-07 11:23:24 +0530

    data analysis day2

--- crimedataanalysis.r
+++ crimedataanalysis.r
@@ -26,3 +26,50 @@ View(s)
 
 LocationDescription = table(mvt$LocationDescription)
 View(LocationDescription)
+
+# 20221007 date
+
+mvt$Date = as.character(mvt$Date)
+DateConvert = as.Date(strptime(mvt$Date,format = "%m/%d/%y/%H:%M"))
+DateConvert = as.Date(strptime(mvt$Date, format = "%m/%d/%y %H:%M"))
+# Check the summary
+
+summary(DateConvert)
+
+mvt$Month = months(DateConvert)
+mvt$Month = as.factor(mvt$Month)
+
+mvt$Day = format(as.Date(DateConvert,format="%Y-%m-%d"), format = "%d")
+mvt$Day = as.factor(mvt$Day)
+
+mvt$Weekday = weekdays(DateConvert)
+mvt$Weekday = as.factor(mvt$Weekday)
+
+mvt$Date = DateConvert
+
+
+summary(mvt$Weekday)
+summary(mvt$Day)
+summary(mvt$Month)
+View(mvt)
+monthsdata =table(mvt$Month)
+View(monthsdata)
+arrestmonth = table(mvt$Arrest,mvt$Month)
+View(arrestmonth)
+hist(mvt$Date, breaks = 100)
+boxplot(mvt$Date ~ mvt$Arrest)
+table(mvt$Arrest,mvt$Year)
+2152/18517
+sortedlocations = sort(table(mvt$LocationDescription))
+View(sortedlocations)
+sortedlocations = sort(table(mvt$LocationDescription), decreasing = TRUE)
+View(sortedlocations)
+toplocations = c("STREET","PARKING LOT/GARAGE(NON.RESID.)")
+TOP2 = subset(mvt, LocationDescription %in% toplocations)
+TOP2$LocationDescription = factor(TOP2$LocationDescription)
+table(TOP2$LocationDescription, TOP2$Arrest)
+table(TOP2$LocationDescription, TOP2$Weekday)      
+TOP2arrests = table(TOP2$LocationDescription, TOP2$Arrest)
+View(TOP2arrests)
+TOP2arrests = as.data.frame(table(TOP2$LocationDescription, TOP2$Arrest))
+View(TOP2arrests)
